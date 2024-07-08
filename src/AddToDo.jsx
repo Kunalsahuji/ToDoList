3a44a1bafc6aaf092aca4bfe262c8474600e80c5
Kunalsahuji <ksahu0103@gmail.com>
2024-07-08 23:28:19 +0530

    Completed

--- src/AddToDo.jsx
+++ src/AddToDo.jsx
@@ -1,3 +1,29 @@
+// import React from 'react'
+// import { useState } from "react";
+// function AddTodo({ addTodo }) {
+//   const [value, setValue] = useState("");
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (!value) return;
+//     addTodo(value);
+//     setValue("");
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//       />
+//       <button type="submit">Add Todo</button>
+//     </form>
+//   );
+// }
+
+// export default AddTodo;
+
 import { useState } from "react";
 function AddTodo({ addTodo }) {
   const [value, setValue] = useState("");
